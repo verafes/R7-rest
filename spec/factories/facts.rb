ffc16dd4e4ede9ca9dd90c8a389d15e87e5bb0d4
verafes <verafes@gmail.com>
2024-09-14 21:57:11 -0700

    Lesson 13. Building an API Part 2

---
+++ spec/factories/facts.rb
@@ -0,0 +1,9 @@
+require 'faker'
+
+FactoryBot.define do
+  factory :fact do
+    fact_text { Faker::ChuckNorris.fact }
+    likes { Faker::Number.number(digits: 3).to_i }
+    association :member
+  end
+end
